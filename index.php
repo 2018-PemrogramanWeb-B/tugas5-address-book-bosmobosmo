d69390a790df42e4aa7c5dbe3b55ea70a477157b
bosmobosmo <36439009+bosmobosmo@users.noreply.github.com>
2018-10-02 12:33:46 +0700

    insert select edit delete

--- index.php
+++ index.php
@@ -9,14 +9,45 @@ if ($conn->connect_error){
   die ('Connection failed: ' .$conn->connect_error);
 }
 
-$sql = "INSERT INTO addressbook (name, address)
-        VALUES ('short', 'darjo')";
+//insert:
+// $sql = "INSERT INTO addressbook (name, address)
+//         VALUES ('short', 'darjo')";
 
-if ($conn->query($sql) === TRUE ){
-  echo 'masuk pak eko';
-} else {
-  echo 'Error: ' .$sql.'<br>'.$conn->error;
-}
+
+// if ($conn->query($sql) === TRUE ){
+//   echo 'masuk pak eko';
+// } else {
+//   echo 'Error: ' .$sql.'<br>'.$conn->error;
+// }
+
+//select:
+// $sql = "SELECT id, name, address FROM  addressbook";
+// $result=$conn->query($sql);
+//
+// if($result->num_rows > 0){
+//   while ($row=$result->fetch_assoc()) {
+//     echo 'id: '.$row['id'].' - Name: '.$row['name'].' - Address: '.$row['address'].'<br>';
+//   }
+// } else {
+//   echo 'kosong pak eko';
+// }
+
+
+//update:
+// $sql="UPDATE addressbook SET address='malang' WHERE  id=2";
+// if ($conn->query($sql) === TRUE){
+//   echo 'masuk pak eko';
+// } else {
+//   echo 'error'.$conn->error;
+// }
+
+//delete:
+// $sql="DELETE FROM addressbook WHERE id=2";
+// if ($conn->query($sql) === TRUE){
+//   echo 'ilang pak eko';
+// } else {
+//   echo 'error'.$conn->error;
+// }
 
 // echo 'connected';
 ?>
